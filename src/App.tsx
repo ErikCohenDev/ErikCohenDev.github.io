2c76ee09658aaba591d6d8ca86c91e3751624062
Erik Cohen <erik@ecohen.dev>
2021-07-28 20:43:34 -0500

    add linting and prettier and editorconfig and change up the homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
+import styled from 'styled-components';
 import './App.css';
+
+const StyledName = styled.span`
+  color: #e31c6d;
+  font-family: 'open-sans';
+`
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <h1>
-          Hello, I'm Erik Cohen,
+          Hello, I'm <StyledName>Erik Cohen</StyledName>
         </h1>
         <p>I'm a Web Developer</p>
       </header>
