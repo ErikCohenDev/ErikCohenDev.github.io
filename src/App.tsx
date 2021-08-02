ffa54b24cc6f3a48b145c4600e79078c12bccc01
Erik Cohen <erik@ecohen.dev>
2021-08-02 16:38:33 -0500

    ga4

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 import './App.css';
+import { GA4R } from 'ga-4-react';
 
 const StyledName = styled.span`
   color: #e31c6d;
@@ -8,14 +9,16 @@ const StyledName = styled.span`
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>
-          Hello, I&apos;m <StyledName>Erik Cohen</StyledName>
-        </h1>
-        <p>I&apos;m a Web Developer</p>
-      </header>
-    </div>
+    <GA4R code={process.env.GA_ANALYTICS as string}>
+      <div className="App">
+        <header className="App-header">
+          <h1>
+            Hello, I&apos;m <StyledName>Erik Cohen</StyledName>
+          </h1>
+          <p>I&apos;m a Web Developer</p>
+        </header>
+      </div>
+    </GA4R>
   );
 }
 
--- src/analytics.ts
+++
@@ -1,6 +0,0 @@
-import ReactGA from 'react-ga';
-
-if (process.env.GA_TOKEN) {
-  ReactGA.initialize(process.env.GA_TOKEN);
-  ReactGA.pageview(window.location.pathname + window.location.search);
-}
